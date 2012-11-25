59fb2d51a37c13320cee5cc56b71739738d965c6
Mark Philpot <mark.philpot@gmail.com>
2012-11-25 12:15:06 -0800

    Updating install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "Installing into $HOME:"
 
 PWD=`pwd`
-FILES="bashrc gitconfig vimrc vim oh-my-zsh zshrc screenrc Xresources"
+FILES="bashrc gitconfig vimrc vim oh-my-zsh zshrc screenrc Xdefaults"
 
 for i in $FILES
 do
@@ -15,3 +15,7 @@ done
 echo "awesome"
 rm -rf $HOME/.config/awesome
 ln -s $PWD/awesome $HOME/.config/awesome
+
+echo "Xresources"
+rm -rf $HOME/.Xresources
+ln -s $PWD/solarized/xresources-colors-solarized/Xresources $HOME/.Xresources
