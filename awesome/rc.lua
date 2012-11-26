ae2e8640d34b4857e355a40dd05250e12aea7ce5
Mark Philpot <mark.philpot@gmail.com>
2012-11-25 21:37:36 -0800

    Added some notification help for layouts and master/columns commands

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -87,10 +87,10 @@ layouts =
     awful.layout.suit.tile.top,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
+    --awful.layout.suit.spiral,
+    --awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
+    --awful.layout.suit.max.fullscreen,
     awful.layout.suit.magnifier
 }
 -- }}}
@@ -265,12 +265,18 @@ globalkeys = awful.util.table.join(
 
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
-    awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-    awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
-    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
-    awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
-    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
+    awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)
+                                                    naughty.notify({ title = 'Master', text = tostring(awful.tag.getnmaster()), timeout = 1 }) end),
+    awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)
+                                                    naughty.notify({ title = 'Master', text = tostring(awful.tag.getnmaster()), timeout = 1 }) end),
+    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)
+                                                    naughty.notify({ title = 'Columns', text = tostring(awful.tag.getncol()), timeout = 1}) end),
+    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)
+                                                    naughty.notify({ title = 'Columns', text = tostring(awful.tag.getncol()), timeout = 1}) end),
+    awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1)
+                                                    naughty.notify({ title = 'Layout', text = awful.layout.getname(), timeout = 1 }) end),
+    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1)
+                                                    naughty.notify({ title = 'Layout', text = awful.layout.getname(), timeout = 1}) end),
 
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
