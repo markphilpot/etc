02a47b5808534de2ae4225c0467b765ffa6e3ff2
Mark Philpot <mark.philpot@gmail.com>
2012-11-26 18:16:08 -0800

    Making the solarized dark bg darker

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "Installing into $HOME:"
 
 PWD=`pwd`
-FILES="bashrc gitconfig vimrc vim oh-my-zsh zshrc screenrc Xdefaults fonts.conf"
+FILES="bashrc gitconfig vimrc vim oh-my-zsh zshrc screenrc Xdefaults Xresources fonts.conf"
 
 for i in $FILES
 do
@@ -15,10 +15,6 @@ done
 echo "awesome"
 rm -rf $HOME/.config/awesome
 ln -s $PWD/awesome $HOME/.config/awesome
-
-echo "Xresources"
-rm -rf $HOME/.Xresources
-ln -s $PWD/solarized/xresources-colors-solarized/Xresources $HOME/.Xresources
 
 echo "Konsole Solarized Dark"
 cp $PWD/lib/konsole-colors-solarized/Solarized\ Dark.colorscheme $HOME/.kde/share/apps/konsole/SolarizedDark.colorscheme
