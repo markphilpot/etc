62ecc9decc1ad94994b6ce1f0aa4ca8179d25bcd
Mark Philpot <mark.philpot@daedafusion.com>
2014-10-09 08:03:20 -0700

    Updating fab scripts

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import with_statement
 from fabric.api import *
+from fabric.contrib import *
 
 def digitalOceanHosts():
     env.hosts = [
@@ -10,3 +11,10 @@ def digitalOceanHosts():
 def update():
     sudo('apt-get update')
     sudo('apt-get -y upgrade')
+
+def checkReboot():
+    if files.exists('/var/run/reboot-required'):
+        print("Reboot required")
+
+def install(apps):
+    sudo('apt-get -y install %s' % apps)
